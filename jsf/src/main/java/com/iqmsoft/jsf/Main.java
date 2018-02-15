5965c353a30dcef2a418fca8ed48fb863f9853d5
Davanon <davanon2014@gmail.com>
2018-02-15 11:02:49 +0530

    First

---
+++ jsf/src/main/java/com/iqmsoft/jsf/Main.java
@@ -0,0 +1,24 @@
+package com.iqmsoft.jsf;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
+import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.undertow.WARArchive;
+
+
+public class Main
+{
+    public static void main(String... args) throws Exception
+    {
+        final Container container = new Container();
+
+        final WARArchive warArchive = ShrinkWrap.create(WARArchive.class);
+        warArchive.addAsWebResource(
+                new ClassLoaderAsset("index.xhtml", Main.class.getClassLoader()), "index.xhtml");
+        warArchive.addAsWebInfResource(
+                new ClassLoaderAsset("WEB-INF/web.xml", Main.class.getClassLoader()), "web.xml");
+        warArchive.addAllDependencies();
+
+        container.start().deploy(warArchive);
+    }
+}
